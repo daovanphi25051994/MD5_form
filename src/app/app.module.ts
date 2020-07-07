94a41f0b80c2e395f987617e238f68a33ac7d785
phi dao <daovanphi25051994@gmail.com>
2020-07-07 16:43:39 +0700

    create login form and register form

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormUserComponent } from './form-user/form-user.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormUserComponent,
+    RegisterComponent,
+    LoginFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
